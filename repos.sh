864ded0850dbd11f81f04aba3fe655ca40c44512
semcovici <pedrosemcovici@outlook.com>
2024-06-18 09:38:51 -0300

    inicial

---
+++ repos.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/nilc-nlp/AMR-BP.git
+git clone https://github.com/LALIC-UFSCar/AMRScien-Br-Corpus.git
+git clone https://github.com/ablodge/amr-utils.git
+git clone https://github.com/inoueMashuu/POS-tagger-portuguese-nltk.git
+
+
+
+
